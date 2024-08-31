cba9f830a07a7d2a57c168f3cf212a3320129cfe
un-identified <namansrivastava1608@gmail.com>
2024-08-31 13:14:40 +0530

    Backend in progress

---
+++ backend.js
@@ -0,0 +1,33 @@
+const http = require('https');
+
+const options = {
+  hostname: 'opentdb.com',
+  path: '/api.php?amount=10&category=26&difficulty=easy&type=multiple',
+  method: 'GET',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+};
+
+const getPosts = () => {
+  let data = '';
+
+  const request = http.request(options, (response) => {
+    response.setEncoding('utf8');
+    response.on('data', (chunk) => {
+      data += chunk;
+    });
+
+    response.on('end', () => {
+      console.log(data);
+    });
+  });
+
+  request.on('error', (error) => {
+    console.error(error);
+  });
+
+  request.end();
+};
+
+getPosts();
